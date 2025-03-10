c7ac8cecd8ade3e0dd16477f2d66be3113b3ab0a
Atahan <atahan012000@gmail.com>
2025-03-10 15:04:25 -0600

    cleanup v1

--- Beginning mood/Assets/Tesselation/Shaders/BeastBakedLitMetaPass.hlsl
+++
@@ -1,9 +0,0 @@
-// Beast - Advanced Tessellation Shader <http://u3d.as/JxL>
-// Copyright (c) Amazing Assets <https://amazingassets.world>
- 
-#ifndef UNIVERSAL_BAKEDLIT_META_PASS_INCLUDED
-#define UNIVERSAL_BAKEDLIT_META_PASS_INCLUDED
-
-#include "Packages/com.unity.render-pipelines.universal/Shaders/UnlitMetaPass.hlsl"
-
-#endif
